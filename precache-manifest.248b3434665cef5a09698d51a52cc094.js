11908036e8011f4326797c67426c91f687d5b473
zeeshanzahoor786 <shawnlawrence786@gmail.com>
2022-08-02 21:03:16 +0200

    Updates

---
+++ precache-manifest.248b3434665cef5a09698d51a52cc094.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "51d4ea1d34f81f6f238a",
+    "url": "/zeeshanzahoor786/citycafe.git/static/css/main.d66fac20.chunk.css"
+  },
+  {
+    "revision": "51d4ea1d34f81f6f238a",
+    "url": "/zeeshanzahoor786/citycafe.git/static/js/main.51d4ea1d.chunk.js"
+  },
+  {
+    "revision": "2948003bcf75af264fbd",
+    "url": "/zeeshanzahoor786/citycafe.git/static/js/1.2948003b.chunk.js"
+  },
+  {
+    "revision": "ebdaab1991e40eb6ed71",
+    "url": "/zeeshanzahoor786/citycafe.git/static/js/runtime~main.ebdaab19.js"
+  },
+  {
+    "revision": "55333c6c6b9ed5586e6a51c1a114822f",
+    "url": "/zeeshanzahoor786/citycafe.git/static/media/meal.55333c6c.mp4"
+  },
+  {
+    "revision": "d719065bb2726afe77e90cec8ea81d5a",
+    "url": "/zeeshanzahoor786/citycafe.git/static/media/bg.d719065b.png"
+  },
+  {
+    "revision": "219184f6a9686a4292803ac25dfc1dbc",
+    "url": "/zeeshanzahoor786/citycafe.git/static/media/chef.219184f6.png"
+  },
+  {
+    "revision": "f9cab310918f2f0f75cd6c9718d347b5",
+    "url": "/zeeshanzahoor786/citycafe.git/static/media/G.f9cab310.png"
+  },
+  {
+    "revision": "5a3891e8d95239f82c09715ae11941b6",
+    "url": "/zeeshanzahoor786/citycafe.git/static/media/gallery01.5a3891e8.png"
+  },
+  {
+    "revision": "2dd1c3366e4ba309e1ab5ef1c4d31f2d",
+    "url": "/zeeshanzahoor786/citycafe.git/static/media/gallery02.2dd1c336.png"
+  },
+  {
+    "revision": "3706b6f7f3577e20cbc4bec6e7698eb5",
+    "url": "/zeeshanzahoor786/citycafe.git/static/media/gallery03.3706b6f7.png"
+  },
+  {
+    "revision": "f51d9759a0055fffba51cb9949949771",
+    "url": "/zeeshanzahoor786/citycafe.git/static/media/gallery04.f51d9759.png"
+  },
+  {
+    "revision": "e96f9bd45b8e0b34eeb03e93f547ccf8",
+    "url": "/zeeshanzahoor786/citycafe.git/static/media/knife.e96f9bd4.png"
+  },
+  {
+    "revision": "5c00c519489e1238ee18cc42a47f141a",
+    "url": "/zeeshanzahoor786/citycafe.git/static/media/logo.5c00c519.png"
+  },
+  {
+    "revision": "6bc82dbcdeb1ed1e9734bed84fccdafb",
+    "url": "/zeeshanzahoor786/citycafe.git/static/media/menu.6bc82dbc.png"
+  },
+  {
+    "revision": "179769474d7506d29c78a6bec834b00c",
+    "url": "/zeeshanzahoor786/citycafe.git/static/media/overlaybg.17976947.png"
+  },
+  {
+    "revision": "72422a1acc4f48e3d82ae3bfa6d1b9d8",
+    "url": "/zeeshanzahoor786/citycafe.git/static/media/spoon.72422a1a.svg"
+  },
+  {
+    "revision": "c29040b5b33e4c9ca3e6b1bb1a2de1d1",
+    "url": "/zeeshanzahoor786/citycafe.git/static/media/welcome.c29040b5.png"
+  },
+  {
+    "revision": "03ef4140ada413449ce1d79046217392",
+    "url": "/zeeshanzahoor786/citycafe.git/static/media/findus.03ef4140.png"
+  },
+  {
+    "revision": "6a5ba623b9ab0bee64d97eef286ab77b",
+    "url": "/zeeshanzahoor786/citycafe.git/static/media/laurels.6a5ba623.png"
+  },
+  {
+    "revision": "a807ceab88095e0cbece581d1600f52d",
+    "url": "/zeeshanzahoor786/citycafe.git/static/media/award01.a807ceab.png"
+  },
+  {
+    "revision": "ff49ca57a8d44a4596c62d4966eb5788",
+    "url": "/zeeshanzahoor786/citycafe.git/static/media/award02.ff49ca57.png"
+  },
+  {
+    "revision": "73a36a9eac74bab796a0e1c799a7ad4a",
+    "url": "/zeeshanzahoor786/citycafe.git/static/media/award03.73a36a9e.png"
+  },
+  {
+    "revision": "d13582bfb80458cf3abd2268d5b5b581",
+    "url": "/zeeshanzahoor786/citycafe.git/static/media/award05.d13582bf.png"
+  },
+  {
+    "revision": "f1363e1deeb3ee759b3a365913a97c85",
+    "url": "/zeeshanzahoor786/citycafe.git/static/media/sign.f1363e1d.png"
+  },
+  {
+    "revision": "04d92e8e1b9c83de6ff362db5ecc9cfd",
+    "url": "/zeeshanzahoor786/citycafe.git/index.html"
+  }
+];
